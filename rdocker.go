60d895e9eb9e8e9455f555e17d31a05d8bab6c10
Fish <zxfishhack@126.com>
2019-01-06 02:18:32 +0800

    init commit

--- rdocker.go
+++ rdocker.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/http/httputil"
 	"time"
 )
 
@@ -35,20 +36,57 @@ func main() {
 
 func (r *rdocker) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	//req.URL.Host = "unix://"
-	req.RequestURI = ""
 	req.URL.Scheme = "http"
 	req.URL.Host = "/var/run/docker.sock"
-	log.Print(req.Header)
-	resp, err := r.client.Do(req)
-	if err != nil {
-		w.WriteHeader(http.StatusBadGateway)
-		_, _ = w.Write([]byte(err.Error()))
-		return
+
+	if req.Header.Get("Upgrade") == "tcp" && req.Header.Get("Connection") == "Upgrade" {
+		//hijack
+		hj, ok := w.(http.Hijacker)
+		if !ok {
+			http.Error(w, "webserver doesn't support hijacking", http.StatusInternalServerError)
+			return
+		}
+
+		b, err := httputil.DumpRequestOut(req, true)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		log.Print(string(b))
+
+		cc, err := net.Dial("unix", "/var/run/docker.sock")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadGateway)
+			return
+		}
+
+		conn, bufrw, err := hj.Hijack()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// Don't forget to close the connection:
+		defer conn.Close()
+
+		cc.Write(b)
+		go io.Copy(cc, conn)
+		go io.Copy(bufrw, cc)
+		select {
+		case <-req.Context().Done():
+		}
+	} else {
+		resp, err := r.client.Do(req)
+		if err != nil {
+			w.WriteHeader(http.StatusBadGateway)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+		for k := range resp.Header {
+			w.Header().Add(k, resp.Header.Get(k))
+		}
+		w.WriteHeader(resp.StatusCode)
+		_, _ = io.Copy(w, resp.Body)
+		_ = resp.Body.Close()
 	}
-	for k := range resp.Header {
-		w.Header().Add(k, resp.Header.Get(k))
-	}
-	w.WriteHeader(resp.StatusCode)
-	_, _ = io.Copy(w, resp.Body)
-	_ = resp.Body.Close()
 }
